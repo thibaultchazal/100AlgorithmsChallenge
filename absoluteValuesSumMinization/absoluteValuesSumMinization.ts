c658b506e70cc3e156c82e9539f83b0b8e16604a
thibaultchazal <chazalthibault@gmail.com>
2019-05-08 20:21:18 +0200

    Solved absoluteValuesSumMinization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,13 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+    if ( a.length % 2 == 0) {
+        x = Math.floor(a.length / 2) - 1;
+    }else{
+        x = Math.floor(a.length / 2);
+    }
+    return (a[x]);
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
