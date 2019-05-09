5718e8d9a570dfab81f8e8753267da2cb85c0575
thibaultchazal <chazalthibault@gmail.com>
2019-05-09 09:58:58 +0200

    Solved

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,10 +1,12 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-    if ( a.length % 2 == 0) {
-        x = Math.floor(a.length / 2) - 1;
-    }else{
-        x = Math.floor(a.length / 2);
-    }
-    return (a[x]);
+    // if ( a.length % 2 == 0) {
+    //     x = Math.floor(a.length / 2) - 1;
+    // }else{
+    //     x = Math.floor(a.length / 2);
+    // }
+    // return (a[x]);
+    even = a.length % 2 == 0
+    return even ? a[Math.floor(a.length / 2) - 1] : a[Math.floor(a.length / 2)]
 }
 
 console.log(absoluteValuesSumMinimization([2, 4, 7]));
