bdf578d3d784f38d2f64a8b35313444b97a55e95
thibaultchazal <chazalthibault@gmail.com>
2019-05-16 09:50:39 +0200

    Solved

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,17 @@
 function addBorder(picture: string[]): string[] {
+    const wall = '*'.repeat(picture[0].length + 2)
+    
+    for(let p in picture){
+        picture[p] = "*" + picture[p] + "*"
+        // picture[p] = '*'.concat(picture[p], '*')
+    }
 
+    picture.splice(0, 0, wall)
+    // picture.unshift(wall)
+    picture.splice(picture.length, 0, wall)
+    // picture.push(wall)
+
+    return picture
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
