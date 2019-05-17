3e0d2995ab16d04fc5d1c6b202705639a7e957a3
thibaultchazal <chazalthibault@gmail.com>
2019-05-17 14:22:04 +0200

    Solved

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,14 @@
 function addTwoDigits(n: any): number {
+    // const n1 = n.toString()[0]
+    // const n2 = n.toString()[1]   
+    // return parseInt(n1) + parseInt(n2)
 
+    const nums = n.toString().split('')
+    
+    return nums.reduce((n1: string, n2: string) => {
+        return parseInt(n1) + parseInt(n2)
+    }) 
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+console.log(addTwoDigits(292));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,10 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+    let products: number[] = []
+    
+    for(let i: number = 0; i < inputArray.length - 1 ; i++){
+        products.push(inputArray[i] * inputArray[i+1])
+    }
+    return Math.max(...products)
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
