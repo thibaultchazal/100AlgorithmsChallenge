95caac161aa4995443ca06163a188ccfb61c6b06
thibaultchazal <chazalthibault@gmail.com>
2019-05-25 16:52:23 +0200

    Solved

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,11 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-
+    let nbFalse: number = 0
+    for(let i:number = 1; i < sequence.length ; i++){
+        sequence[i] > sequence[i-1] ? '' : nbFalse++
+    }
+    return nbFalse > 1 ? false : true
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 2, 3])) 
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,10 @@
 function alphabeticShift(inputString: string): string {
-
+    let inputShifted = inputString.split('')
+    for(let i:number = 0; i < inputShifted.length ; i++){
+        let n = inputString.charCodeAt(i)
+        inputShifted[i] = n === 122 ? String.fromCharCode(97) : String.fromCharCode(n+1)
+    }
+    return inputShifted.join('')
 }
 
 console.log(alphabeticShift('crazy'));
