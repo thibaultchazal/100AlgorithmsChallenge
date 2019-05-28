07fe72b20bfbefbffc5dffede991b2e370750155
thibaultchazal <chazalthibault@gmail.com>
2019-05-28 20:29:08 +0200

    Solved

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,10 @@
 function alphabetSubsequence(s: string): boolean {
-
+    let alphabeticOrder = true
+    for(let i:number = 1; i < s.length ; i++){
+        alphabeticOrder = s.charCodeAt(i) > s.charCodeAt(i - 1) ? alphabeticOrder : false
+        // inputShifted[i] = n === 122 ? String.fromCharCode(97) : String.fromCharCode(n+1)
+    }
+    return alphabeticOrder
 }
 
 console.log(alphabetSubsequence('zab'))
